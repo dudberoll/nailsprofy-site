977ae7fa1777148dd06861dc85048d820f932ee4
dud <dudberoll@gmail.com>
2026-07-21 16:43:21 +0300

    Обновить тексты и контакты главной страницы

--- website/src/data/homeContent.ts
+++ website/src/data/homeContent.ts
@@ -2,7 +2,7 @@ export const serviceCards = [
 	{ title: "Маникюр", image: "/images/services/manicure-closeup.png", href: "/services#manicure" },
 	{ title: "Педикюр", image: "/images/services/pedicure-spa.png", href: "/services#pedicure" },
 	{ title: "Волосы", image: "/images/services/hair-care.png", href: "/services#hair" },
-	{ title: "Брови", image: "/images/services/brows-portrait.png", href: "/services#brows" },
+	{ title: "Брови/Ресницы", image: "/images/services/brows-portrait.png", href: "/services#brows" },
 	{ title: "Ресницы", image: "/images/services/lashes-portrait.jpg", href: "/services#lashes" },
 	{ title: "Эпиляция", image: "/images/services/epilation-smooth-legs.jpg", href: "/services#epilation" },
 	{ title: "Обучение", image: "/images/services/training-manicure-session.jpg", href: "/services#training" },
